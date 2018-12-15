7bced2ba8cb02bd0c03321d2b70e57a7a8da0798
m2web <m2web@yahoo.com>
2018-12-14 20:46:50 -0500

    Updated file to display object properties and replace a + with a space.

--- uriEncodedTest.js
+++ uriEncodedTest.js
@@ -1,10 +1,13 @@
-//decode to string "id=7&name=Pablo+Matías&lastname=Gomez&githubUsername=pablomatiasgomez"
-var decodedString = decodeURI("id=7&name=Mark+Mat%C3%ADas&lastname=McFadden&githubUsername=m2web");
+//decode to string "id=7&firstname=Pablo+Matías&lastname=Gomez&githubUsername=pablomatiasgomez"
+var decodedString = decodeURI("id=7&firstname=Mark+Clifford&lastname=McFadden&githubUsername=m2web");
 
 //Next do the following with the decodedString
 //Escape " (quotes): stays the same as there are no quotes
-//Replace & (ampersand) with a , (comma): id=7,name=Mark+Matías,lastname=McFadden,githubUsername=m2web
-//Replace = (equals) with a semicolon (:)  id:7,name:Mark+Matías,lastname=M:Fadden,githubUsername:m2web
-//Suround with curly brakets and quotes: { id: '7', name: 'Mark+Matías', lastname: 'McFadden', githubUsername: 'm2web' }
+//Replace & (ampersand) with a , (comma): id=7,firstname=Mark+Clifford,lastname=McFadden,githubUsername=m2web
+//Replace = (equals) with a semicolon (:)  id:7,firstname:Mark+Clifford,lastname=M:Fadden,githubUsername:m2web
+//Suround with curly brakets and quotes: { id: '7', firstname: 'Mark+Clifford', lastname: 'McFadden', githubUsername: 'm2web' }
 var result = JSON.parse('{"' + decodedString.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"')  + '"}');
-console.log(result);
+console.log("id: " + result.id);
+console.log("firstname: " + result.firstname.replace(/\+/g,' '));//replace + with a space
+console.log("lastname: " + result.lastname.replace(/\+/g,' '));
+console.log("githubUsername: " + result.githubUsername.replace(/\+/g,' '));
